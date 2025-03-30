25a447c632be5c4ace675f10b2cebaa2047cc482
mukul_verma <aryanv018@gmail.com>
2025-03-30 14:19:50 +0530

    Refactored custom JavaScript action to use 'require' instead of 'import' for module loading and changed log level from info to notice for better output clarity.

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,9 @@
-const core = import('@actions/core')
-const github = import('@actions/github')
-const exec = import('@actions/exec')
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
 
 function run(){
-    core.info("Hello from my custom javascript Action!")
+    core.notice("Hello from my custom javascript Action!")
 }
 
 run()
